0935134b10176ca1873dc716e2dc9c588aeb0377
Guillaume Crégut <gcregut@free.fr>
2024-02-07 15:42:12 +0100

    [Feat]: add autoload

--- app/src/classes/Autoloader.php
+++ app/src/classes/Autoloader.php
@@ -11,11 +11,13 @@ class Autoloader
 
     static function autoload($class)
     {
+        //NameSpace Editiel98
         if (strpos($class, __NAMESPACE__ . '\\') === 0) {
             $class = str_replace(__NAMESPACE__ . '\\', '', $class);
             $class = str_replace('\\', '/', $class);
             require '../src/classes/' . $class . '.php';
         }
+        //NameSpace App
         if (strpos($class, 'App' . '\\') === 0) {
             $class = str_replace('App' . '\\', '', $class);
             $class = str_replace('\\', '/', $class);
