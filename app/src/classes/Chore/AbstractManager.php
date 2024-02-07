79e85ad3b02e83cb1d99be2bcbe28c0d441e4dd9
Guillaume Crégut <gcregut@free.fr>
2024-02-07 17:59:30 +0100

    [Feat]: AbstractController: add exec and prepare function

--- app/src/classes/Chore/AbstractManager.php
+++ app/src/classes/Chore/AbstractManager.php
@@ -3,6 +3,9 @@
 namespace Editiel98\Chore;
 
 use Editiel98\Chore\Database;
+use Editiel98\Chore\Exception\DbException;
+use Editiel98\Flash;
+use Exception;
 
 /**
  * Manage informations from DB to Entity
@@ -47,4 +50,51 @@ abstract class AbstractManager
         $method = 'get' . ucfirst($name);
         return $this->$method();
     }
+
+    /**
+     * Exec the query
+     *
+     * @param string $query : Query to execute
+     * @param array $vars : vars for the query
+     * @return mixed
+     */
+    protected function execSQL(string $query, array $vars): mixed
+    {
+        try {
+            $result = $this->db->exec($query, $vars);
+            return $result;
+        } catch (DbException $e) {
+            if ($e->getDbCode() === 23000) {
+                $flash = new Flash();
+                $flash->setFlash('Modification impossible', 'error');
+                return false;
+            }
+            $message = 'SQL : ' . $query . 'a poser problème';
+            $emitter = Emitter::getInstance();
+            $emitter->emit(Emitter::DATABASE_ERROR, $message);
+            throw new Exception('Une erreur est survenue');
+        }
+    }
+
+    /**
+     * Execute a prepared query
+     *
+     * @param string $query : query to execute
+     * @param array $vars : vars for the query
+     * @param boolean $single : return one result or not
+     * @return mixed
+     */
+    protected function prepareSQL(string $query, array $vars, bool $single): mixed
+    {
+        try {
+            $result = $this->db->prepare($query, $this->className, $vars, $single);
+            return $result;
+        } catch (DbException $e) {
+            $message = 'SQL : ' . $query . 'a poser problème';
+            $emitter = Emitter::getInstance();
+            $emitter->emit(Emitter::DATABASE_ERROR, $message);
+            throw new Exception('Une erreur est survenue');
+        }
+    }
+
 }
