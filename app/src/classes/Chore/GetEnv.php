fe37693623e78aa6c0d8a38bf83e9c7aa10817a4
Guillaume Crégut <gcregut@free.fr>
2024-02-07 15:47:33 +0100

    [Feat]: remove function from GetEnv

--- app/src/classes/Chore/GetEnv.php
+++ app/src/classes/Chore/GetEnv.php
@@ -5,22 +5,6 @@ use Exception;
 
 class GetEnv
 {
-    public static function getEnv(string $iniFile, ?string $section='')
-    {
-        $envDatas=getenv();
-        if(!empty ($envDatas['login'])){
-
-        }
-        $envs=parse_ini_file($iniFile,true);
-        if(!$envs) {
-            throw new Exception('Configuration file missing');
-        }
-        $envValues=(object) $envs;
-        if($section!=='') {
-            return $envValues->$section;
-        }
-    }
-
     public static function getEnvValue(string $value)
     {
         $iniFile='../../.env';
