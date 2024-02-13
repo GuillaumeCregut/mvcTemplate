ae5cd92917b06a675b01f531c77174f0f177994a
Guillaume Crégut <gcregut@free.fr>
2024-02-13 14:32:23 +0100

    [Feat]: RouteAttribute: add datas

--- app/src/Core/Chore/Attribute/RouteAttribute.php
+++ app/src/Core/Chore/Attribute/RouteAttribute.php
@@ -7,9 +7,26 @@ use Attribute;
 class RouteAttribute
 {
     public function __construct(
-        private string $path
+        private string $path,
+        private ?array $datas=[]
     )
     {
         
     }
+
+        /**
+         * Get the value of path
+         */
+        public function getPath(): string
+        {
+                return $this->path;
+        }
+
+        /**
+         * Get the value of datas
+         */
+        public function getDatas(): ?array
+        {
+                return $this->datas;
+        }
 }
