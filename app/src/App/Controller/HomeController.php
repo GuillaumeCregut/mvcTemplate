b912ada2397f1e57c878f9ca6ad77a59e7a0d45a
Guillaume Crégut <gcregut@free.fr>
2024-02-13 16:35:30 +0100

    [Feat]: Home Controller : add route attribute

--- app/src/App/Controller/HomeController.php
+++ app/src/App/Controller/HomeController.php
@@ -2,10 +2,11 @@
 namespace App\Controller;
 
 use Editiel98\AbstractController;
-
+use Editiel98\Chore\Attribute\RouteAttribute;
 
 class HomeController extends AbstractController
 {
+    #[RouteAttribute('/')]
     public function index(): void
     {
        $this->smarty->display('_base.tpl');
