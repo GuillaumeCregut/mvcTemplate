372f0a0fa4cd06efa617208c49423f17cd4c2259
Guillaume Crégut <gcregut@free.fr>
2024-02-13 19:32:10 +0100

    [Feat]: RoutesDisplay: forget to save

--- app/src/Core/Kernel/Routing/RoutesDisplay.php
+++ app/src/Core/Kernel/Routing/RoutesDisplay.php
@@ -62,19 +62,6 @@ class RoutesDisplay
      * 
      * @return array
      */
-   /* private function checkUrlVars(string $url): array|false
-    {
-        $slug = '';
-        if ($start = strpos($url, '{')) {
-            if (strpos($url, '}')) {
-                $slug = substr($url, $start + 1, -1);
-                $length=strlen($url)-$start;
-                $path= substr($url,0,$length-1);
-                return array('slug'=>$slug,'path'=>$path);
-            }
-        }
-        return false;
-    }*/
 
     /**
      * Get the value of routes
