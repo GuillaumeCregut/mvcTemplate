1df2201c810d06356e8c7ffcdd93dc7f0c7ce10f
Guillaume Crégut <gcregut@free.fr>
2024-02-13 23:20:57 +0100

    [Refacto]: RouteDisplay: refactoring class

--- app/src/Core/Kernel/Routing/RoutesDisplay.php
+++ app/src/Core/Kernel/Routing/RoutesDisplay.php
@@ -7,61 +7,13 @@ use ReflectionClass;
 
 class RoutesDisplay
 {
-    use SlugTrait;
 
     private array $routes = [];
 
     public function __construct()
     {
-        $controllers = ClassFinder::getClassesInNamespace('App\\Controller');
-        var_dump($controllers);
-        foreach ($controllers as $controller) {
-            if (get_parent_class($controller) === 'Editiel98\AbstractController') {
-                $controllerRoutes = $this->registerContoller($controller);
-                if (!$controllerRoutes) {
-                    continue;
-                }
-                var_dump($controllerRoutes);
-                foreach($controllerRoutes as $controllerRoute) {
-                    $this->routes[$controllerRoute['name']]=$controllerRoute['path'];
-                }
-            }
-        }
-        var_dump($this->routes);
+       $this->routes= RegisterController::getRoutes();
     }
-
-    private function registerContoller(string $controller): array |false
-    {
-        var_dump($controller);
-        $routes = [];
-        $class = new ReflectionClass($controller);
-        foreach ($class->getMethods() as $method) {
-            if (empty($method->getAttributes(\Editiel98\Kernel\Attribute\RouteAttribute::class)))
-                continue;
-            $methodName = $method->getName();
-            $routeAttributes[$methodName] = $method->getAttributes(\Editiel98\Kernel\Attribute\RouteAttribute::class)[0];
-        }
-        if (empty($routeAttributes)) {
-            return false;
-        }
-        foreach ($routeAttributes as $key => $routeAttribute) {
-            $route = $routeAttribute->newInstance();
-            $routeName = $route->getName();
-            $path=$route->getPath();
-            $slugs= $this->checkUrlVars($path);
-            if($slugs) {
-                $path=$slugs['path'];
-            }
-            $routes[] = array('name' => $routeName, 'path' => $path);
-        }
-        return $routes;
-    }
-
-    /**
-     * @param string $url
-     * 
-     * @return array
-     */
 
     /**
      * Get the value of routes
@@ -70,4 +22,17 @@ class RoutesDisplay
     {
         return $this->routes;
     }
+
+    /**
+     * @param string $route
+     * 
+     * @return string
+     */
+    public function getRoute(string $route):string |null
+    {
+        if(key_exists($route,$this->routes)){
+            return $this->routes[$route];
+        } else return null;
+    }
+    
 }
