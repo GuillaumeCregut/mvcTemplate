2a1a63a9938b950e391daa1e138b818338226e49
Guillaume Crégut <gcregut@free.fr>
2024-02-13 10:13:43 +0100

    [Feat]: TraitEntity: create

---
+++ app/src/Core/Chore/Entity/TraitEntity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Editiel98\Chore\Entity;
+
+use Exception;
+
+trait TraitEntity
+{
+    public function createFromData(array $datas)
+    {
+        var_dump($datas);
+        try{
+        foreach ($datas as $name => $field) {
+            if (property_exists($this::class, $name)) {
+                $this->$name = $field;
+            } else {
+                echo "Passe pas $name";
+            }
+        }
+    } catch(Exception $e) {
+        var_dump($e->getMessage());
+    }
+    }
+}
