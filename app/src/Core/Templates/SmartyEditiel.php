0351253ae470237dccc919686c1f14aafa28cae4
Guillaume Crégut <gcregut@free.fr>
2024-02-13 23:14:58 +0100

    [Feat]:SmartyEditiel: add plugin support

--- app/src/Core/Templates/SmartyEditiel.php
+++ app/src/Core/Templates/SmartyEditiel.php
@@ -7,6 +7,9 @@
 
 namespace Editiel98\Templates;
 
+use Editiel98\Kernel\ClassFinder;
+use ReflectionClass;
+
 require_once(dirname((__FILE__)) . '/smarty/Smarty.class.php');
 
 class SmartyEditiel extends \Smarty
@@ -17,6 +20,19 @@ class SmartyEditiel extends \Smarty
       $baseDir = __DIR__ . '/../../';
       $this->template_dir = $baseDir . 'templates/';
       $this->compile_dir = $baseDir . 'templates_c/';
-      $this->assign('app_name', 'Models Kit Database');
+      $this->getPlugin();
+   }
+
+   private function getPlugin()
+   {
+      $plugins = ClassFinder::getClassesInNamespace('Editiel98\\Templates');
+      foreach ($plugins as $plugin) {
+         if (get_parent_class($plugin) === 'Editiel98\Templates\AbstractPlugin') {
+            $class = new ReflectionClass($plugin);
+            $r = $class->newInstance();
+            $name = $r->getName();
+            $this->registerPlugin('function', $name, [$r, 'display']);
+         }
+      }
    }
 }
