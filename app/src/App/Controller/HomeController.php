aab4212e10679cb29c7cd736d1a2f39ed7517d5b
Guillaume Crégut <gcregut@free.fr>
2024-02-13 23:18:02 +0100

    [Style]: HomeController: remove unused elements

--- app/src/App/Controller/HomeController.php
+++ app/src/App/Controller/HomeController.php
@@ -3,14 +3,12 @@ namespace App\Controller;
 
 use Editiel98\AbstractController;
 use Editiel98\Kernel\Attribute\RouteAttribute;
-use Editiel98\Kernel\Routing\RoutesDisplay;
 
 class HomeController extends AbstractController
 {
     #[RouteAttribute('/', name:'home')]
     public function index(): void
     {
-        $router=new RoutesDisplay();
         $this->smarty->display('_base.tpl');
     }
 }
