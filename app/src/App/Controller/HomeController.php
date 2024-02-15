b9fc416e78f15b895ccd7390156c031263c606a1
Guillaume Crégut <gcregut@free.fr>
2024-02-15 15:41:39 +0100

    [Fix]: HomeController: change render method

--- app/src/App/Controller/HomeController.php
+++ app/src/App/Controller/HomeController.php
@@ -4,15 +4,12 @@ namespace App\Controller;
 
 use Editiel98\AbstractController;
 use Editiel98\Kernel\Attribute\RouteAttribute;
-use Editiel98\Kernel\Routing\RoutesDisplay;
 
 class HomeController extends AbstractController
 {
     #[RouteAttribute('/', name: 'home')]
     public function index(): void
     {
-        $toto=new RoutesDisplay();
-        var_dump($toto->getRoutes());
-        $this->smarty->display('_base.tpl');
+        $this->render('_base.tpl');
     }
 }
