703bd996ac51472e13eb9cba8d71c1785cd8681d
Guillaume Crégut <gcregut@free.fr>
2024-02-19 16:22:50 +0100

    [Test]: CSRF Token

---
+++ app/tests/Kernel/CsrfCheckTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Editiel98\Kernel\CSRFCheck;
+use Editiel98\Session;
+use PHPUnit\Framework\TestCase;
+
+class CsrfCheckTest extends TestCase
+{
+    public function testSetToken(): void
+    {
+        $session=new Session();
+        $csrf=new CSRFCheck($session);
+        $token=$csrf->createToken();
+        $tokenKey=$session->getKey('token_part');
+        $this->assertIsString($token);
+        $this->assertIsString($tokenKey);
+    }
+
+    public function testGetTokenWithoutInit(): void
+    {
+        $session=new Session();
+        $csrf=new CSRFCheck($session);
+        $token=$csrf->checkToken('');
+        $this->assertEquals(false,$token);
+    }
+
+    public function testGetTokenWithoutToken(): void
+    {
+        $session=new Session();
+        $csrf=new CSRFCheck($session);
+        $token=$csrf->createToken();
+        $token=$csrf->checkToken('');
+        $this->assertEquals(false,$token);
+    }
+
+    public function testGetTokenWithWrongToken(): void
+    {
+        $session=new Session();
+        $csrf=new CSRFCheck($session);
+        $token=$csrf->createToken();
+        $token=$csrf->checkToken('123456');
+        $this->assertEquals(false,$token);
+    }
+
+    public function testGetTokenWithOKToken(): void
+    {
+        $session=new Session();
+        $csrf=new CSRFCheck($session);
+        $token=$csrf->createToken();
+        $tokenResult=$csrf->checkToken($token);
+        $this->assertEquals(true,$tokenResult);
+    }
+}
