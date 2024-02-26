a0d5c8fe493dd02f1834b088526f6bc85e0ffa69
Guillaume Crégut <gcregut@free.fr>
2024-02-26 19:06:59 +0100

    [Refacto]: AbstractEntity: refactoring class

--- app/src/Core/Kernel/Entity/AbstractEntity.php
+++ app/src/Core/Kernel/Entity/AbstractEntity.php
@@ -2,8 +2,6 @@
 
 namespace Editiel98\Kernel\Entity;
 
-use Exception;
-
 /**
  * Abstract class Entity
  */
@@ -12,14 +10,6 @@ abstract class AbstractEntity
     use TraitEntity;
 
     protected int $id;
-    /**
-     * @var array<mixed>
-     */
-    protected array $datas = [];
-
-    public function __construct()
-    {
-    }
 
     /**
      * Get the value of id
@@ -37,41 +27,5 @@ abstract class AbstractEntity
         $this->id = $id;
 
         return $this;
-    }
-
-    /**
-     * @return array<mixed>
-     */
-    abstract public function getDatasForForm(): array;
-
-    /**
-     * @param mixed $name
-     *
-     * @return mixed
-     */
-    public function __get($name): mixed
-    {
-        $method = 'get' .  ucfirst($name);
-        if (method_exists($this, $method)) {
-            return $this->$method();
-        } else {
-            return null;
-        }
-    }
-
-    /**
-     * @param mixed $name
-     * @param mixed $value
-     *
-     */
-    public function __set($name, $value)
-    {
-        echo "Set $name a la valeur $value";
-        $method = 'set' . ucfirst($name);
-        if (method_exists($this, $method)) {
-            return $this->$method($value);
-        } else {
-            throw new Exception('Method not found');
-        }
     }
 }
