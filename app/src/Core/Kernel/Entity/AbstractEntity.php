ab5c9b102bafba759e015ea2279efefc41ee10d1
Guillaume Crégut <gcregut@free.fr>
2024-02-26 19:17:17 +0100

    [Refacto]: AbstractEntity: remove unused params

--- app/src/Core/Kernel/Entity/AbstractEntity.php
+++ app/src/Core/Kernel/Entity/AbstractEntity.php
@@ -7,25 +7,4 @@ namespace Editiel98\Kernel\Entity;
  */
 abstract class AbstractEntity
 {
-    use TraitEntity;
-
-    protected int $id;
-
-    /**
-     * Get the value of id
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set the value of id
-     */
-    public function setId(int $id): self
-    {
-        $this->id = $id;
-
-        return $this;
-    }
 }
