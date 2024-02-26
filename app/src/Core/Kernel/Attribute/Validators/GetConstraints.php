6432e5e442d4eb34cfee0f2c5759dc4477e6dca4
Guillaume Crégut <gcregut@free.fr>
2024-02-26 17:29:37 +0100

    [Feat]: GetConstraints: create static class

---
+++ app/src/Core/Kernel/Attribute/Validators/GetConstraints.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Editiel98\Kernel\Attribute\Validators;
+
+use Editiel98\Kernel\Entity\AbstractEntity;
+use ReflectionAttribute;
+use ReflectionClass;
+
+class GetConstraints
+{
+    /**
+     * @var mixed[]
+     */
+    private static array $constraints = [];
+
+
+    /**
+     * @param AbstractEntity $entity
+     *
+     * @return mixed[]
+     */
+    public static function scanEntity(AbstractEntity $entity): array
+    {
+        $entityReflector = new ReflectionClass($entity);
+        foreach ($entityReflector->getProperties() as $property) {
+            self::$constraints[$property->name] = [];
+            if (!empty($property->getAttributes(
+                \Editiel98\Kernel\Attribute\Constraints\AbstractConstraints::class,
+                ReflectionAttribute::IS_INSTANCEOF
+            ))) {
+                self::addAttribute($property->name, $property->getAttributes());
+            }
+        }
+        return self::$constraints;
+    }
+
+    /**
+     * @param mixed[] $attributes
+     *
+     * @return void
+     */
+    private static function addAttribute(string $name, array $attributes): void
+    {
+        foreach ($attributes as $attribute) {
+            self::$constraints[$name][] = $attribute;
+        }
+    }
+}
